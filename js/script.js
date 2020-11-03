0673859053793e10ecf7de1f777101180bc6be72
WillV1 <wmvanhook@gmail.com>
2020-11-03 13:48:00 -0500

    Add floating button and modal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-// const navToggle = document.querySelector('.nav-toggle');
-// const navLinks = document.querySelectorAll('.nav-link');
+// Get the modal
+const modal = document.getElementById("myModal");
 
-// navToggle.addEventListener('click', () => {
-//   document.body.classList.toggle('nav-open');
-// });
+// Get the button that opens the modal
+const btn = document.querySelector(".floating");
 
-// navLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     document.body.classList.remove('nav-open');
-//   })
-// })
+// Get the <span> element that closes the modal
+const span = document.querySelector(".close");
 
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+btn.addEventListener("click", () => {
+  modal.style.display = "block"
+});
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+span.addEventListener("click", () => {
+  modal.style.display = "none"
+});
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+window.addEventListener("click", (e) => {
+  if(e.target == modal) {
+    modal.style.display = "none";
+  }
+})
 
