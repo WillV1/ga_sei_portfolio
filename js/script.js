0d095fddd792f90720f2159d34a02fb3ea54a575
WillV1 <wmvanhook@gmail.com>
2020-11-06 20:04:37 -0500

    Update project section

--- js/script.js
+++ js/script.js
@@ -8,29 +8,18 @@ const btn = document.querySelector(".floating");
 const span = document.querySelector(".close");
 
 // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
 
 btn.addEventListener("click", () => {
   modal.style.display = "block"
 });
 
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
 
 span.addEventListener("click", () => {
   modal.style.display = "none"
 });
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
 
 window.addEventListener("click", (e) => {
   if(e.target == modal) {
